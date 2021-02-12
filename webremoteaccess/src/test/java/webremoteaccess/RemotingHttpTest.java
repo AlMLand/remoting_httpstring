8a65d26d27e098b36a3011be6c4ac8cd5bf1c4ba
AlMLand <morlandalex@googlemail.com>
2021-02-12 22:28:58 +0100

    Modified the test class, removed the logger and class show.

--- webremoteaccess/src/test/java/webremoteaccess/RemotingHttpTest.java
+++ webremoteaccess/src/test/java/webremoteaccess/RemotingHttpTest.java
@@ -6,8 +6,6 @@ import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -20,8 +18,6 @@ import com.m_landalex.webremoteaccess.service.EmployeeService;
 @RunWith(SpringRunner.class)
 public class RemotingHttpTest {
 
-	private static final Logger logger = LoggerFactory.getLogger(RemotingHttpTest.class);
-	
 	@Autowired
 	private EmployeeService employeeService;
 	
@@ -34,12 +30,7 @@ public class RemotingHttpTest {
 	@Test
 	public void contentCheck() {
 		Employee returnedEmployee = employeeService.fetchById(1L);
-		show(returnedEmployee);
 		assertEquals("McGregor", returnedEmployee.getLastName());
-	}
-	
-	private void show(Employee employee) {
-		logger.info("Employee: {}", employee.toString());
 	}
 	
 }
